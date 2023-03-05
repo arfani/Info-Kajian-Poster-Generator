98bda6f0e1c0586b1747d02e9b9166e1ebb65198
arfani <arfanihidayat@gmail.com>
2023-03-06 00:47:32 +0800

    update link to create poster

--- components/hero.tsx
+++ components/hero.tsx
@@ -6,8 +6,8 @@ export default function Hero() {
             <div className="mx-auto text-center ">
                 <h1 className="my-14 text-4xl">Buat Poster Info Kajian Anda Tanpa <strong className="line-through">Desain</strong> !</h1>
                 <div className="flex justify-center sm:justify-start">
-                    <Link title="Cooming Soon" href="#" className="px-6 py-2 bg-secondary text-white border-r-2 border-primary hover:bg-slate-600">Buat Poster <br /><span className="text-xs text-yellow-200">Cooming soon</span></Link>
-                    <Link href="/info-kajian-lombok" className="flex flex-col justify-center p-4 bg-secondary text-white hover:bg-slate-600">Info Kajian Lombok</Link>
+                    <Link href="/create-poster" className="p-4 bg-secondary text-white border-r-2 border-primary hover:bg-slate-600">Buat Poster</Link>
+                    <Link href="/info-kajian-lombok" className="p-4 bg-secondary text-white hover:bg-slate-600">Info Kajian Lombok</Link>
                 </div>
 
             </div>
